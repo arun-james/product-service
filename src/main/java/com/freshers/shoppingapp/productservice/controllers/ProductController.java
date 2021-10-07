6661f3a8d7bfac4c88f0da319447ed3d6032f70f
Arun James <arunworkmail@gmail.com>
2021-10-07 15:50:14 +0530

    added product controller

---
+++ src/main/java/com/freshers/shoppingapp/productservice/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package com.freshers.shoppingapp.productservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProduct() {
+        return "Prodcut";
+    }
+}
