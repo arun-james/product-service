f0b91165b2746c36d826a82957fe38b917d52aaf
Arun James <arunworkmail@gmail.com>
2021-10-07 15:44:16 +0530

    initial commit

---
+++ src/test/java/com/freshers/shoppingapp/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshers.shoppingapp.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
